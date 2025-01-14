9a5bbeaa863c1f6dfb228b515c758be4994e8cbc
Majd Q <majdqassem12@gmail.com>
2025-01-14 23:21:03 +0200

    Finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './componoents/App.jsx'
+import React from "react";
+import App from "./componoents/App";
+import "../src/index.css";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById('root')).render(
-    <App />
-)
+      <App />
+);
